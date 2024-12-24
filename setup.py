734e65d744e21d81b4664d0fdccb9d4136d0a3c5
jona4git <ivvi4coolpad@gmail.com>
2024-12-24 22:02:56 +0200

    requirements-added & installation done

--- setup.py
+++ setup.py
@@ -20,12 +20,12 @@ with open("README.md", "r", encoding="utf-8") as f:
         long_description_content_type="text/markdown",
         url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
         packages=setuptools.find_packages(where="src"),
-        # package_dir={"": "src"},
-        # classifiers=[
-        #     "Programming Language :: Python :: 3",
-        #     "License :: OSI Approved :: MIT License",
-        #     "Operating System :: OS Independent",
-        # ],
+        package_dir={"": "src"},
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
         # python_requires=">=3.6",
         # install_requires=[
         #     "transformers==4.18.0",
