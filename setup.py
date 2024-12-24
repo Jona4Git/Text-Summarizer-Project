a7946974b32ef139482a1684823772987a0f3271
jona4git <ivvi4coolpad@gmail.com>
2024-12-24 21:14:03 +0200

    folder structure added

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+    __version__ = "0.0.0"
+
+    REPO_NAME = "Text-Summarizer-Project"
+    AUTHOR_USER_NAME = "Jonathan"
+    SRC_REPO = "textSummarizer"
+    AUTHOR_EMAIL = "ivvi4coolpad@gmail.com"
+
+    setuptools.setup(
+        name=SRC_REPO,
+        version=__version__,
+        author=AUTHOR_USER_NAME,
+        author_email=AUTHOR_EMAIL,
+        description="A text summarization tool using the Transformers library. A small python package for NLP app",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+        packages=setuptools.find_packages(where="src"),
+        # package_dir={"": "src"},
+        # classifiers=[
+        #     "Programming Language :: Python :: 3",
+        #     "License :: OSI Approved :: MIT License",
+        #     "Operating System :: OS Independent",
+        # ],
+        # python_requires=">=3.6",
+        # install_requires=[
+        #     "transformers==4.18.0",
+        #     "torch==1.10.0",
+        #     "torchvision==0.11.1",
+        # ]
+    )
--- template.py
+++ template.py
@@ -3,19 +3,19 @@ from pathlib import Path
 import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "textSummerizer"
+project_name = "textSummarizer"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    f"src/(project_name)/__init__.py",
-    f"src/(project_name)/components/__init__.py",
-    f"src/(project_name)/utilis/__init__.py",
-    f"src/(project_name)/utilis/common.py",
-    f"src/(project_name)/logging//__init__.py",
-    f"src/(project_name)/config/configuration.py",
-    f"src/(project_name)/pipeline/__init__.py",
-    f"src/(project_name)/entity/__init__.py",
-    f"src/(project_name)/constants/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utilis/__init__.py",
+    f"src/{project_name}/utilis/common.py",
+    f"src/{project_name}/logging//__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "params.yaml",
     "app.py",
@@ -26,11 +26,11 @@ list_of_files = [
     "research/trials.ipynb",
     "acs_master_upb.py",
 ]
-
+ 
 # The logic to create the structure
 for filepath in list_of_files:
     filepath = Path(filepath) 
-    filedir, filename =os.path.split(filepath)
+    filedir, filename = os.path.split(filepath)
 
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
@@ -42,4 +42,4 @@ for filepath in list_of_files:
             logging.info(f"Creating empty file: {filepath}")
 
         else:
-            logging.info(f"{filename} already exists")
+            logging.info(f"{filename} already exists") 
